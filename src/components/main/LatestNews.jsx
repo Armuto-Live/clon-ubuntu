26132d127790613c1ce2174a94b78a556ccbc87b
Armuto-Live <68910378+Armuto-Live@users.noreply.github.com>
2024-01-12 22:37:45 -0500

    LatestNews component v1

--- src/components/main/LatestNews.jsx
+++ src/components/main/LatestNews.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { GridRow, LayoutMaxWidth, PStrip } from "../global";
-import { styled, Typography } from "@mui/material";
+import { styled, Typography,Box } from "@mui/material";
 import { NavigationLogoLink } from "../../style";
 const Title = styled("h3")(({ theme }) => ({}));
 
@@ -10,12 +10,33 @@ export const LatestNews = () => {
       <LayoutMaxWidth>
         <GridRow xxs="span 4" sm="span 6" md="span 12">
           <Typography variant="h3">
-            Latest news from 
+            Latest news from
             <NavigationLogoLink>&nbsp;our  blog {"›"}</NavigationLogoLink>
           </Typography>
         </GridRow>
-        <GridRow xxs="span 4" sm="span 6" md="span 12">
-          asd
+      </LayoutMaxWidth>
+      <LayoutMaxWidth>
+        <GridRow xxs="span 4" sm="span 6" md="span 9" sx={{display:"flex"}}>
+          <GridRow md="span 3">
+            <p>12 January 2024</p>
+            <span>Cloud-native infrastructure – When the future meets the present</span>
+          </GridRow>
+          <GridRow md="span 3">
+            <p>12 January 2024</p>
+            <span>Cloud-native infrastructure – When the future meets the present</span>
+          </GridRow>
+          <GridRow md="span 3">
+            <p>12 January 2024</p>
+            <span>Cloud-native infrastructure – When the future meets the present</span>
+          </GridRow>
+        </GridRow>
+        <GridRow xxs="span 4" sm="span 6" md="span 3" sx={{display:"flex","&:before":{content:'""',color:"red",height:"100%",border:"thin solid red",marginRight:"1rem"}}} >
+          <GridRow >
+            <Box>
+              <p>12 January 2024</p>
+              <span>Cloud-native infrastructure – When the future meets the present</span>
+            </Box>
+          </GridRow>
         </GridRow>
       </LayoutMaxWidth>
     </PStrip>
